4c99203beb2842166d8dcec70b4fdfb6daaeb017
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-11-10 12:27:34 -0800

    adding perf console logs

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,9 +5,15 @@ export default async function fetchAPI(path: string) {
   const url = path.startsWith("user") ? user(path) : story(path);
   const headers = { "User-Agent": "chrome" };
 
+  const hrstart = process.hrtime();
+  let reshrend;
+  let texthrend;
+
   try {
     let response = await fetch(url, { headers });
+    reshrend = process.hrtime(hrstart);
     let text = await response.text();
+    texthrend = process.hrtime(hrstart);
     try {
       if (text === null) {
         return { error: "Not found" };
@@ -20,5 +26,10 @@ export default async function fetchAPI(path: string) {
     }
   } catch (error) {
     return { error };
+  } finally {
+    const jsonhrend = process.hrtime(hrstart);
+    console.info('resp  Execution time (hr): %ds %dms', reshrend[0], reshrend[1] / 1000000);
+    console.info('text  Execution time (hr): %ds %dms', texthrend[0], texthrend[1] / 1000000);
+    console.info('json  Execution time (hr): %ds %dms', jsonhrend[0], jsonhrend[1] / 1000000);
   }
 }
